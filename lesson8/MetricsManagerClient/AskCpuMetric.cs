3021a247a44fea038a6f1fad737fea4901dfb102
Сергей <saygon707@gmail.com>
2021-08-08 22:28:47 +0300

    правки

---
+++ lesson8/MetricsManagerClient/AskCpuMetric.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Text.Json;
+
+namespace MetricsManagerClient
+{
+    class AskCpuMetric
+    {
+        private readonly HttpClient httpClient = new HttpClient();
+        private string connectionString = "http://localhost:5000";
+        private int agentId = 0;
+        public AskCpuMetric()
+        {
+
+        }
+
+        public List<int> GetMetric(CpuRequest request)
+        {
+            var fromParameter = request.FromTime.TotalSeconds;
+            var toParameter = request.ToTime.TotalSeconds;
+            var httpRequest = new HttpRequestMessage(HttpMethod.Get, $"{connectionString}/api/agent/{agentId}/from/{fromParameter}/to/{toParameter}");
+            List<int> result = new List<int>();
+
+            try
+            {
+                HttpResponseMessage response = httpClient.SendAsync(httpRequest).Result;
+                using var responseStream = response.Content.ReadAsStreamAsync().Result;
+                foreach (var metric in JsonSerializer.DeserializeAsync<AllCpuMetricsResponse>(responseStream).Result.Metrics)
+                {
+                    result.Add(metric.Value);
+                }
+            }
+            catch
+            {
+
+            }
+            return result;
+        }
+
+    }
+}
---
+++ lesson8/MetricsManagerClient/MaterialCards.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using LiveCharts;
+using LiveCharts.Wpf;
+using System.ComponentModel;
+
+namespace MetricsManagerClient
+{
+    /// <summary>
+    /// Логика взаимодействия для MaterialCards.xaml
+    /// </summary>
+    public partial class MaterialCards : UserControl, INotifyPropertyChanged
+    {
+        public double hours = 12;
+        public MaterialCards()
+        {
+            InitializeComponent();
+
+            ColumnSeriesValues = new SeriesCollection
+            {
+                new ColumnSeries
+                {
+                    Values = new ChartValues<int> (new AskCpuMetric().GetMetric(
+                        new CpuRequest{ 
+                            FromTime = new TimeSpan(DateTimeOffset.UtcNow.Subtract(TimeSpan.FromHours(hours)).ToUnixTimeSeconds()),
+                            ToTime = new TimeSpan(DateTimeOffset.UtcNow.ToUnixTimeSeconds())
+                        }))
+                }
+            };
+
+            DataContext = this;
+        }
+
+        public SeriesCollection ColumnSeriesValues { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void UpdateOnСlick(object sender, RoutedEventArgs e)
+        {
+            TimePowerChart.Update(true);
+        }
+    }
+}
